eeceedbd4f582ab5ed4a47589141e2a2963fc99b
rodalcham <142218652+rodalcham@users.noreply.github.com>
2024-07-03 11:42:20 +0200

    fixed strlen

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: rchavez <rchavez@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 17:42:09 by rchavez           #+#    #+#             */
-/*   Updated: 2024/06/25 14:54:55 by rchavez          ###   ########.fr       */
+/*   Updated: 2024/07/03 11:42:03 by rchavez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	count;
 
 	count = 0;
+	if (!s)
+		return (0);
 	while (s[count])
 	{
 		count++;
